86fa380281cec05e1940e02113e50db715897230
axiiom <camallanyoung@gmail.com>
2020-05-06 14:49:57 -0400

    working!

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from "react";
 import SyncLoader from "react-spinners/SyncLoader";
-import { Container, Grid } from "@material-ui/core";
+import { Container, Grid, TextField, IconButton } from "@material-ui/core";
 
+import CheckIcon from '@material-ui/icons/Check';
 
 import Temperature from "./components/temperature";
-import Target from "./components/target";
-import PowerButton from "./components/power";
 
-
-const ws = new WebSocket('ws://192.168.50.184:3005')
-
+const ws = new WebSocket("ws://192.168.50.184:3005");
 
 const classes = {
   margin: 0,
@@ -19,7 +16,6 @@ const classes = {
   transform: "translate(-50%,-50%)",
 };
 
-
 class App extends React.Component {
   super(props) {
     this.props = props;
@@ -27,7 +23,7 @@ class App extends React.Component {
       target: null,
       temperature: null,
       deadzone: null,
-      on: true
+      on: true,
     };
   }
 
@@ -35,36 +31,43 @@ class App extends React.Component {
     const msg = JSON.stringify({ action: "receive" });
     ws.onopen = () => {
       console.log("websocket connected");
-      setTimeout(() => {ws.send(msg)}, 500)
+      ws.send(msg);
     };
 
     ws.onmessage = (event) => {
       console.log(event);
-      const data = JSON.parse(event.data)
+      const { temperature, target, deadzone, on } = JSON.parse(event.data);
+      console.log(
+        `temp = ${temperature}\ntarget = ${target}\ndeadzone = ${deadzone}\non = ${on}`
+      );
+
       this.setState({
-        temperature: parseInt(data.temperature),
-        target: data.target,
-        deadzone: data.deadzone,
-        on: data.on
-      })
+        temperature: parseInt(temperature),
+        target: parseInt(target),
+        deadzone: parseInt(deadzone),
+        on: parseInt(on),
+      });
     };
 
     setInterval(() => {
       if (ws != null) {
+        const update_msg = JSON.stringify({
+          action: "update",
+          target: this.state.target,
+        })
+        
+        ws.send(update_msg);
         console.log(msg);
         ws.send(msg);
+
       }
     }, 5000);
   }
 
-  updateSocket = (e) => {
-    e.preventDefault();
-    console.log(this.state.text);
-    ws.send(JSON.stringify({ action: this.state.text }));
-    this.setState({
-      text: "",
-    });
-  };
+  updateTarget = (e) => {
+    console.log(e.target.value)
+    this.setState({target: e.target.value})
+  }
 
   handleChange = (e) => {
     console.log(this.state.text);
@@ -73,20 +76,19 @@ class App extends React.Component {
     });
   };
 
-
-  /* START HERE */
-
   renderClimate = () => {
     return (
       <>
-        <Grid item xs={3}>
-          <PowerButton on={this.state.on} toggle={this.toggle} />
-        </Grid>
         <Grid item xs={4}>
           <Temperature temperature={this.state.temperature} />
-        </Grid>
-        <Grid item xs={3}>
-          <Target target={this.state.target} />
+          <div style={{ marginTop: 20 }}>
+            <TextField
+              label="Target Temperature"
+              type="number"
+              value={this.state.target}
+              onChange={this.updateTarget}
+            />
+          </div>
         </Grid>
       </>
     );
@@ -102,49 +104,15 @@ class App extends React.Component {
           justify="center"
           direction="row"
         >
-          {this.state == null || this.state.loading ? <SyncLoader color={"#3e98c7"} /> : this.renderClimate()}
+          {this.state == null || this.state.loading ? (
+            <SyncLoader color={"#3e98c7"} />
+          ) : (
+            this.renderClimate()
+          )}
         </Grid>
       </Container>
     );
   }
-  /* END HERE */
-
-
-
-  // render() {
-  //   if (this.state == null) {
-  //     return "loading";
-  //   }
-
-  //   const { temperature, target, deadzone, on } = JSON.parse(
-  //     this.state.message
-  //   );
-  //   const display = (
-  //     <p>
-  //       Temperature: {temperature}
-  //       <br></br>
-  //       Target: {target}
-  //       <br></br>
-  //       Deadzone: {deadzone}
-  //       <br></br>
-  //       On: {on ? "on" : "off"}
-  //     </p>
-  //   );
-
-  //   return (
-  //     <div>
-  //       {display}
-  //       <button type="submit" value="send message" onClick={this.updateSocket}>
-  //         Send Message
-  //       </button>
-  //       <input
-  //         type="text"
-  //         value={this.state.text}
-  //         onChange={this.handleChange}
-  //       ></input>
-  //     </div>
-  //   );
-  // }
 }
 
 export default App;
--- src/components/target.jsx
+++ src/components/target.jsx
@@ -1,23 +1,56 @@
 import CircularSlider from "@fseehawer/react-circular-slider";
 import React from "react";
 
-export default function Target(props) {
-  return (
-    <div style={{ marginTop: "30vh" }}>
-      <CircularSlider
-        width={150}
-        label="Target"
-        hideKnob={true}
-        valueFontSize={"2rem"}
-        verticalOffset={"0.5rem"}
-        dataIndex={props.target - 65}
-        min={65}
-        max={88}
-        trackSize={12}
-        progressSize={12}
-        knobColor="#b931b0"
-        appendToValue="°"
-      />
-    </div>
-  );
+import SyncLoader from "react-spinners/SyncLoader";
+
+export default class Target extends React.Component {
+  super(props) {
+    this.props = props;
+    this.setState({
+      target: props.target,
+    });
+  }
+
+  componentDidMount = () => {
+    this.setState({ target: this.props.target });
+
+    const msg = JSON.stringify({ action: "receive" });
+    setInterval(() => {
+      this.props.websock.send(msg);
+    }, 2000);
+
+    this.props.websock.onmessage = (event) => {
+      const { target } = JSON.parse(event.data);
+      this.setState({
+        target: target,
+      });
+    };
+  };
+
+  updateTarget = (value) => {
+    console.log(value);
+  };
+
+  render() {
+    return this.state == null ? (
+      <SyncLoader color={"#3e98c7"} />
+    ) : (
+      <div style={{ marginTop: "30vh" }}>
+        <CircularSlider
+          width={150}
+          label="Target"
+          valueFontSize={"2rem"}
+          verticalOffset={"0.5rem"}
+          dataIndex={this.state.target - 65}
+          onChange={(value) => this.updateTarget(value)}
+          min={65}
+          max={88}
+          trackSize={12}
+          progressSize={12}
+          knobColor="#b931b0"
+          appendToValue="°"
+        />
+      </div>
+    );
+  }
 }
